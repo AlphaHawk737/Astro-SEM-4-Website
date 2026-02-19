6e1c56124cdb5535d8f553be216a7e76795dcf3e
AlphaHawk737 <aarnav.1kundalia@gmail.com>
2026-02-19 11:37:16 +0530

    Scroll-To-Top Button

--- js/history.js
+++ js/history.js
@@ -95,3 +95,24 @@ window.addEventListener("scroll", () => {
   const scrollPercent = (scrollTop / documentHeight) * 100;
   progressBar.style.width = scrollPercent + "%";
 });
+
+
+
+// Scroll To Top Button
+
+const scrollBtn = document.getElementById("scrollTopBtn");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    scrollBtn.classList.add("show");
+  } else {
+    scrollBtn.classList.remove("show");
+  }
+});
+
+scrollBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
